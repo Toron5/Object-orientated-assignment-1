f0eb0520473dc437c1b72b74a92efe27d3f792ca
Toron5 <57416895+Toron5@users.noreply.github.com>
2022-03-21 12:04:56 +0000

    Commented start

--- Object orientated assignment 1/Start.cs
+++ Object orientated assignment 1/Start.cs
@@ -12,30 +12,29 @@ namespace CMP1903M_Assessment_1_Base_Code
 
         public string UserInput(string Option)
         {
-
+        // Asks the User what option they want to choose
             Console.WriteLine("Pick a number:");
             Console.WriteLine("----------------------");
             Console.WriteLine(" 1: User Input\n 2: File Input\n 3: Quit");
             Console.WriteLine("----------------------");
+        // reads the option and defines it 
+        
             Option = Console.ReadLine();
-            //try
-            //{
-            //    if (Option != "1" && Option != "2" && Option != "3")
-            //    {
-            //        throw new IncorrectInputHandling(Option);
-            //    }
-            return Option;
-            //}
+            while (Option != "1" && Option != "2" && Option != "3" )
+            {
+                // if there isn't a valid input it will loop untill there is
+                Console.WriteLine("Incorrect Input try again");
+                Console.WriteLine(" 1: User Input\n 2: File Input\n 3: Quit");
+                Option = Console.ReadLine();
+            }
+                // returns the value    
+                return Option;
+            
 
 
 
-            //catch (Exception ex)
-            //{
-
-            //    Console.WriteLine("'{0}' is not a valid choice. Please try again.", ex.Message);
-
-            //    return UserInput();
-            //}
+            
+          
         }
 
 
